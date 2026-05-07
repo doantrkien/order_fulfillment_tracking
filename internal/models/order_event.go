bc68e3a15fcdafae532abc9586df27d160fc9ec9
doantrkien <IntAGT26.02.001@agest.vn>
2026-05-07 15:26:00 +0700

    "style: reformat model order and event"

--- internal/models/order_event.go
+++ internal/models/order_event.go
@@ -1,16 +1,18 @@
 package models
 
-import basemodel "main/internal/models/base_model"
-import "time"
+import (
+	basemodel "main/internal/models/base_model"
+	"time"
+)
 
 type OrderEvent struct {
 	basemodel.BaseModel
-    EventID     string      `gorm:"column:event_id;uniqueIndex;not null" json:"event_id"`
-    OrderID     int64       `gorm:"column:order_id;not null;index" json:"order_id"`
-    Order       Order       `gorm:"foreignKey:OrderID" json:"-"`
-    EventType   string      `gorm:"column:event_type;not null" json:"event_type"`
-    NewStatus   OrderStatus `gorm:"column:new_status;not null" json:"new_status"`
-    DriverID    *int64      `gorm:"column:driver_id" json:"driver_id,omitempty"`
-    OccurredAt  time.Time   `gorm:"column:occurred_at" json:"occurred_at"`
-    ProcessedAt time.Time   `gorm:"column:processed_at" json:"processed_at"`
+	EventID     string      `gorm:"column:event_id;uniqueIndex;not null" json:"event_id"`
+	OrderID     int64       `gorm:"column:order_id;not null;index" json:"order_id"`
+	Order       Order       `gorm:"foreignKey:OrderID" json:"-"`
+	EventType   string      `gorm:"column:event_type;not null" json:"event_type"`
+	NewStatus   OrderStatus `gorm:"column:new_status;not null" json:"new_status"`
+	DriverID    *int64      `gorm:"column:driver_id" json:"driver_id,omitempty"`
+	OccurredAt  time.Time   `gorm:"column:occurred_at" json:"occurred_at"`
+	ProcessedAt time.Time   `gorm:"column:processed_at" json:"processed_at"`
 }
