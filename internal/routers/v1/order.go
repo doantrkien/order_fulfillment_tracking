be87c6245ca7fc766d31953e393e2eeeadd6bb50
Nguyen Tien Khoa <ducrua1311@gmail.com>
2026-05-20 13:35:17 +0700

    Fix order route handler registration

--- internal/routers/v1/order.go
+++ internal/routers/v1/order.go
@@ -10,7 +10,7 @@ import (
 func SetupOrderRouter(app *fiber.App, orderHandler *handlers.OrderHandler) {
 	order := app.Group("api/v1/orders", middlewares.Authenticate())
 	//order := app.Group("api/v1/orders")
-	der.Get("", middlewares.Authorize([]string{"admin", "customer", "shipper"}), orderHandler.GetAllOrder)
+	order.Get("", middlewares.Authorize([]string{"admin", "customer", "shipper"}), orderHandler.GetAllOrder)
 	//order.Get("",  orderHandler.GetAllOrder)
 
 	order.Get("/:id", middlewares.Authorize([]string{"admin", "customer", "shipper"}), orderHandler.GetOrderDetail)
