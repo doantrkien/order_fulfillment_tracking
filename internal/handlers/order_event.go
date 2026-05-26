6c1d0e51e95eebb79a7cfef96d6b4047d9a9469f
Khoa Nguyen <ducrua1311@gmail.com>
2026-05-26 14:17:52 +0700

    merge dev

--- internal/handlers/order_event.go
+++ internal/handlers/order_event.go
@@ -36,6 +36,13 @@ func (h *OrderEventHandler) ImportOrderEvents(c fiber.Ctx) error {
 	if err := c.Bind().Body(&req); err != nil {
 		return response.ResponseError(c, errs.ERR_INVALID_INPUT, nil)
 	}
+	const maxBatchSize = 1000
+	if len(req) > maxBatchSize {
+		return response.Reponse(c, 400, "batch too large, max 2000 events", nil)
+	}
+
+	ctx, cancel := context.WithTimeout(c.Context(), 30*time.Second)
+	defer cancel()
 
 	result, err := h.orderEventService.ImportOrderEvents(c, req)
 	if err != nil {
